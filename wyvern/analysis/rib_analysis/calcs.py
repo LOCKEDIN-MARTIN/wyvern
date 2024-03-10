004a5cb67a61bf4fb11b1ad8df2ce75683fbd90f
Mingde Yin <mingdeyin@gmail.com>
2024-03-10 01:32:21 -0500

    twist and secondary spar

--- wyvern/analysis/rib_analysis/calcs.py
+++ wyvern/analysis/rib_analysis/calcs.py
@@ -1,10 +1,19 @@
+from dataclasses import dataclass
 from io import StringIO
 from typing import Callable
 
 import numpy as np
 import numpy.typing as npt
-from matplotlib import pyplot as plt
 from scipy.integrate import quad
+
+
+@dataclass
+class AirfoilPoints:
+    # all arrays should be monotonically increasing
+    x_top: npt.NDArray[np.floating]
+    y_top: npt.NDArray[np.floating]
+    x_bot: npt.NDArray[np.floating]
+    y_bot: npt.NDArray[np.floating]
 
 
 def rib_loading(lift_distribution: Callable[[float], float], rib_y: npt.NDArray):
@@ -31,11 +40,48 @@ def rib_loading(lift_distribution: Callable[[float], float], rib_y: npt.NDArray)
     return rib_force
 
 
+def get_section_coords(
+    section: str, scale_fac: float, twist: float = 0, twist_xc: float = 0.5
+) -> AirfoilPoints:
+    """
+    Get the coordinates of a section.
+    """
+    section_coords = np.loadtxt(StringIO(section)) * scale_fac
+
+    section_x = section_coords[:, 0]
+    section_y = section_coords[:, 1]
+
+    # find breakpoint for top and bottom of section
+    midpt = np.where(section_x == np.min(section_x))[0][0]
+
+    # twist section
+    section_y = section_y * np.cos(twist * np.pi / 180) + (
+        section_x - twist_xc * scale_fac
+    ) * np.sin(twist * np.pi / 180)
+    section_x = (
+        (section_x - twist_xc * scale_fac) * np.cos(twist * np.pi / 180)
+        - (section_y) * np.sin(twist * np.pi / 180)
+        + twist_xc * scale_fac
+    )
+
+    x_top = section_x[: midpt + 1]
+    y_top = section_y[: midpt + 1]
+    x_bot = section_x[midpt:]
+    y_bot = section_y[midpt:]
+
+    # reverse top coordinates
+    x_top = np.flip(x_top)
+    y_top = np.flip(y_top)
+
+    return AirfoilPoints(x_top, y_top, x_bot, y_bot)
+
+
 def spar_height(
     rib_y: npt.NDArray[np.floating],
     rib_c: npt.NDArray[np.floating],
     rib_xle: npt.NDArray[np.floating],
     spar_x: npt.NDArray[np.floating],
+    twist: npt.NDArray[np.floating],
     sections: list[str],
 ) -> tuple[npt.NDArray[np.floating], npt.NDArray[np.floating]]:
     """
@@ -48,45 +94,14 @@ def spar_height(
 
     spar_points = spar_x - rib_xle
 
-    ax = plt.figure().add_subplot(projection="3d")
-    plt.tight_layout()
-
     for i in range(num_ribs):
         # read points from section file
         scale_fac = rib_c[i]
-        section_coords = np.loadtxt(StringIO(sections[i])) * scale_fac
 
-        section_x = section_coords[:, 0]
-        section_y = section_coords[:, 1]
-
-        # find breakpoint for top and bottom of section
-        midpt = np.where(section_x == np.min(section_x))[0][0]
-
-        x_top = section_x[: midpt + 1]
-        y_top = section_y[: midpt + 1]
-        x_bot = section_x[midpt:]
-        y_bot = section_y[midpt:]
-
-        # reverse top coordinates
-        x_top = np.flip(x_top)
-        y_top = np.flip(y_top)
+        airfoil = get_section_coords(sections[i], scale_fac, twist[i])
 
         # interpolate to find y-coordinates of spar intersection
-        spar_tops[i] = np.interp(spar_points[i], x_top, y_top)
-        spar_bots[i] = np.interp(spar_points[i], x_bot, y_bot)
+        spar_tops[i] = np.interp(spar_points[i], airfoil.x_top, airfoil.y_top)
+        spar_bots[i] = np.interp(spar_points[i], airfoil.x_bot, airfoil.y_bot)
 
-        ax.plot(x_top + rib_xle[i], rib_y[i] * np.ones_like(y_top), y_top, "r-")
-        ax.plot(x_bot + rib_xle[i], rib_y[i] * np.ones_like(y_bot), y_bot, "b-")
-        ax.plot(
-            [spar_x[i], spar_x[i]],
-            [rib_y[i], rib_y[i]],
-            [spar_tops[i], spar_bots[i]],
-            "g-",
-        )
-    ax.plot(spar_x, rib_y, spar_tops, "g-")
-    ax.plot(spar_x, rib_y, spar_bots, "g-")
-
-    ax.axis("equal")
-    plt.subplots_adjust(left=-0.8, right=1.7, top=1.7, bottom=-0.7)
-    ax.set_axis_off()
     return (spar_tops, spar_bots)
--- wyvern/analysis/structural_design.py
+++ wyvern/analysis/structural_design.py
@@ -14,6 +14,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 from wyvern.analysis.rib_analysis.calcs import rib_loading, spar_height
+from wyvern.analysis.rib_analysis.plots import rib_spar_structure_plot
 from wyvern.data.airfoils import BOEING_VERTOL, NACA0018
 from wyvern.utils.constants import G
 from wyvern.utils.geom_utils import mirror_verts
@@ -36,6 +37,7 @@ ctrl_c = mirror_verts(np.array([780, 600, 400, 120]), negate=False) * 1e-3  # m
 ctrl_xle = mirror_verts(np.array([0, 0.120, 0.215, 0.598]), negate=False)  # m
 # Sections at each stations
 ctrl_sections = [BOEING_VERTOL] * 3 + [NACA0018] + [BOEING_VERTOL] * 3
+ctrl_twist = np.array([7, 0, 7])  # degrees (for some reason, -ve is +ve)
 
 # Rib Locations
 rib_y = mirror_verts(np.array([0, 92.5, 185, 318, 451, 584, 717, 850])) * 1e-3  # m
@@ -62,7 +64,23 @@ ctrl_spar_x = (
 )  # m; (c/4 centerline to c/4 tip)
 
 # secondary spar
-ctrl_spar2_y = mirror_verts(np.array([185, 850])) * 1e-3  # m
+ctrl_spar2_y = np.array([185, 850]) * 1e-3  # m
+ctrl_spar2_x = np.array([485, 688]) * 1e-3  # m
+# 3c/4 from wings to tip
+
+# hack: linear extrapolate from y=185 to y=0
+ctrl_spar2_x = np.insert(
+    ctrl_spar2_x,
+    0,
+    ctrl_spar2_x[0]
+    + ((ctrl_spar2_x[1]) - (ctrl_spar2_x[0]))
+    / (ctrl_spar2_y[1] - ctrl_spar2_y[0])
+    * (0 - ctrl_spar2_y[0]),
+)
+ctrl_spar2_y = np.insert(ctrl_spar2_y, 0, 0)
+
+ctrl_spar2_y = mirror_verts(ctrl_spar2_y)
+ctrl_spar2_x = mirror_verts(ctrl_spar2_x, negate=False)
 
 # Interpolate to rib locations
 # Chord at each rib
@@ -73,6 +91,9 @@ rib_xle = np.interp(rib_y, ctrl_y, ctrl_xle)
 rib_sections = [BOEING_VERTOL] * 6 + [NACA0018] * 3 + [BOEING_VERTOL] * 6
 # Spar intersection x-coordinates
 spar_x = np.interp(rib_y, ctrl_spar_y, ctrl_spar_x)
+spar_2x = np.interp(rib_y, ctrl_spar2_y, ctrl_spar2_x)
+
+twist = np.interp(rib_y, np.array([-850, 0, 850]) * 1e-3, ctrl_twist)
 
 
 # Lift loading
@@ -84,14 +105,30 @@ def ell(y):
 
 
 rib_force = rib_loading(ell, rib_y)
-rib_s_stop, rib_s_bot = spar_height(rib_y, rib_c, rib_xle, spar_x, rib_sections)
+rib_s_stop, rib_s_bot = spar_height(rib_y, rib_c, rib_xle, spar_x, twist, rib_sections)
+rib_s2_stop, rib_s2_bot = spar_height(
+    rib_y, rib_c, rib_xle, spar_2x, twist, rib_sections
+)
+
+# plot 3D views of spar and rib layout
+rib_spar_structure_plot(
+    [rib_s_stop, rib_s2_stop],
+    [rib_s_bot, rib_s2_bot],
+    rib_y,
+    rib_c,
+    rib_xle,
+    [spar_x, spar_2x],
+    twist,
+    rib_sections,
+)
 
 
 with open("rib_spar_layout.txt", "w") as f:
-    f.write("y\t x\t z_top\t z_bot\t height\n")
+    f.write("y\t x1\t z1_top\t z1_bot\t h1\tx2\t z2_top\t z2_bot\t h2\t\n")
     for i in range(num_ribs):
         f.write(
-            f"{rib_y[i]*1000:.2f}\t {spar_x[i]*1000:.2f}\t {rib_s_stop[i]*1000:.2f}\t {rib_s_bot[i]*1000:.2f}\t {1000*(rib_s_stop[i] - rib_s_bot[i]):.2f}\n"
+            f"{rib_y[i]*1000:.2f}\t {spar_x[i]*1000:.2f}\t {rib_s_stop[i]*1000:.2f}\t {rib_s_bot[i]*1000:.2f}\t {1000*(rib_s_stop[i] - rib_s_bot[i]):.1f} \t"
+            f"{spar_2x[i]*1000:.2f}\t {rib_s2_stop[i]*1000:.2f}\t {rib_s2_bot[i]*1000:.2f}\t {1000*(rib_s2_stop[i] - rib_s2_bot[i]):.1f}\n"
         )
 #
 plt.savefig("3d_structure.pdf")
@@ -101,12 +138,17 @@ plt.gca().view_init(elev=0, azim=0)
 plt.subplots_adjust(left=-0.6, right=1.6, top=1.6, bottom=-0.6)
 plt.savefig("2d_structure.pdf")
 
+# side shot
+plt.gca().view_init(elev=0, azim=90)
+plt.savefig("side_structure.png", dpi=300)
+
 # Plots
 fig, axs = plt.subplots(3, 1, tight_layout=True, figsize=(8, 8))
 
 for i in range(num_ribs):
     axs[0].plot([rib_y[i], rib_y[i]], [rib_xle[i], rib_xle[i] + rib_c[i]], "k-")
-axs[0].plot(rib_y, spar_x, "g-")
+axs[0].plot(rib_y, spar_x, color="C0", label="Main Spar")
+axs[0].plot(rib_y, spar_2x, color="C1", label="Secondary Spar")
 
 ax1_twin = axs[1].twinx()
 ax1_twin.bar(rib_y, rib_force, width=0.05)
@@ -116,13 +158,17 @@ axs[1].plot(
     "r-",
 )
 
-axs[2].plot(rib_y, rib_s_stop, "r-", label="Top of spar")
-axs[2].plot(rib_y, rib_s_bot, "b-", label="Bottom of spar")
+axs[2].plot(rib_y, rib_s_stop, "C0", label="Main Spar")
+axs[2].plot(rib_y, rib_s_bot, "C0", label="_")
+axs[2].plot(rib_y, rib_s2_stop, "C1", label="Secondary Spar")
+axs[2].plot(rib_y, rib_s2_bot, "C1", label="_")
+
 
 axs[0].set_xlabel("Spanwise position (m)")
 axs[0].set_ylabel("Chordwise position (m)")
 axs[0].set_title("Spar and Rib layout")
 axs[0].grid(True)
+axs[0].legend()
 
 axs[1].set_title("Structural Loading (n = 3)")
 axs[1].set_xlabel("Spanwise position (m)")
@@ -139,6 +185,3 @@ axs[2].axis("equal")
 plt.savefig("rib_spar_layout.pdf")
 
 plt.show()
-
-
-# Determine rib height at each spar intersection
